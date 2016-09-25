8f6bbd94f475aa04e65717234c0657412db364b4
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2016-09-25 19:15:58 +0900

    REST Sample

---
+++ src/main/java/com/ignacio/rest/Resource.java
@@ -0,0 +1,16 @@
+package com.ignacio.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class Resource {
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+		String output = "REST say : " + msg;
+		return Response.status(200).entity(output).build();
+	}
+}
