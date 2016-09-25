a3fa9b8820b0f752a6fe3d49ccaab5ca7bf5744b
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2016-09-25 19:20:12 +0900

    Add swagger annotations

--- src/main/java/com/ignacio/rest/Resource.java
+++ src/main/java/com/ignacio/rest/Resource.java
@@ -3,12 +3,19 @@ package com.ignacio.rest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags={"TagToGroupSomeAPIs"})
 @Path("/hello")
 public class Resource {
 	@GET
 	@Path("/{param}")
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
 	public Response getMsg(@PathParam("param") String msg) {
 		String output = "REST say : " + msg;
 		return Response.status(200).entity(output).build();
