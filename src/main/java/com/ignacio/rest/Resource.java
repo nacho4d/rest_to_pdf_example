7410bac3c55929c92bba6d3e8750a4d85d2c8cf7
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2016-09-25 21:27:43 +0900

    Add one endpoint more. It was too lonely

--- src/main/java/com/ignacio/rest/Resource.java
+++ src/main/java/com/ignacio/rest/Resource.java
@@ -1,6 +1,7 @@
 package com.ignacio.rest;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -10,14 +11,24 @@ import javax.ws.rs.core.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(tags={"TagToGroupSomeAPIs"})
+@Api(tags = { "TagToGroupSomeAPIs" })
 @Path("/hello")
 public class Resource {
-	@GET
-	@Path("/{param}")
-	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-	public Response getMsg(@PathParam("param") String msg) {
-		String output = "REST say : " + msg;
-		return Response.status(200).entity(output).build();
-	}
+    @GET
+    @Path("/{param}")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @ApiOperation(value = "Send a message via GET")
+    public Response getMsg(@PathParam("param") String msg) {
+        String output = "REST say : " + msg;
+        return Response.status(200).entity(output).build();
+    }
+
+    @POST
+    @Path("/{param}")
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+    @ApiOperation(value = "Send a message via POST")
+    public Response postMsg(@PathParam("param") String msg) {
+        String output = "REST say : " + msg;
+        return Response.status(200).entity(output).build();
+    }
 }
